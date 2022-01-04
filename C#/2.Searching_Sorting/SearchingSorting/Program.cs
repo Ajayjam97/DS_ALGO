d21ddd1e8a494e98a95dd99c3640dc4214551a6e
AjayJam97 <jamwal_ajay@hotmail.com>
2022-01-04 22:54:32 +0530

    C# Linear Search

--- C#/2.Searching_Sorting/SearchingSorting/Program.cs
+++ C#/2.Searching_Sorting/SearchingSorting/Program.cs
@@ -4,9 +4,32 @@ namespace SearchingSorting
 {
     class Program
     {
+
+        public static bool LinearSearch(int[] arr, int num){
+
+            for(int i=0; i<arr.Length; i++){
+                if(arr[i]==num) return true;
+            }
+
+            return false;
+        }
+
+        public static void Searching(){
+
+            int[] arr = {1,2,3,4,5};
+            int num = 9;
+
+            bool found = LinearSearch(arr, num);
+
+            Console.WriteLine(string.Join(",", arr));
+            Console.WriteLine("{0} found", (found)?"Yes!":"No, not");
+
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Searching();
         }
     }
 }
