d25fd39725cc4730efef47a1b02a810cb97e3d40
AjayJam97 <jamwal_ajay@hotmail.com>
2022-01-05 23:20:48 +0530

    BubbleSort

--- C++/2.Searching_Sorting/3.BubbleSort.cpp
+++ C++/2.Searching_Sorting/3.BubbleSort.cpp
@@ -4,12 +4,12 @@ using namespace std;
 void BubbleSort(int arr[],int n){
 
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(arr[j]>arr[j+1])
+        for(int j=1; j<(n-i); j++){
+            if(arr[j]<arr[j-1])
             {
                 int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
             }
             
         }
