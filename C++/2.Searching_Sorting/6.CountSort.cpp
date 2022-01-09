9b9295b130eebc4c723a33a8a164eb1311456795
AjayJam97 <jamwal_ajay@hotmail.com>
2022-01-09 18:39:07 +0530

    RangeCountSort

--- C++/2.Searching_Sorting/6.CountSort.cpp
+++ C++/2.Searching_Sorting/6.CountSort.cpp
@@ -21,7 +21,30 @@ void CountSort(int arr[],int n, int hi){
        
 }
 
+void RangeCountSort(int arr[],int n, int hi, int lo){
+
+    int fmap[hi-lo+1];
+    memset(fmap, 0, sizeof(fmap));
+
+    for(int i=0; i<n; i++){
+        int index=arr[i];
+        fmap[index-lo]++;
+    }
+
+    int j=0;
+    for(int i=0; i<(sizeof(fmap)/sizeof(int)); i++){
+        while(fmap[i]--){
+            arr[j]=i+lo;
+            j++;
+        }
+    }
+
+}
+        
+   
+
 int main(){
+
     int arrsize; cin>>arrsize;
     int arr[arrsize];
 
@@ -29,11 +52,13 @@ int main(){
         cin>>arr[i];
     }
 
-    int hi; cin>>hi;
-    CountSort(arr, arrsize, hi);
+    int lo,hi; cin>>lo>>hi;
+    //CountSort(arr, arrsize, hi);
+    RangeCountSort(arr, arrsize, hi, lo);
 
     for(int i=0; i<(sizeof(arr)/sizeof(int)); i++){
         cout<<arr[i]<<" ";
     }
+
     
 }
