ed778f70c9ee4778652ede16cb2bd5a772d25829
AjayJam97 <jamwal_ajay@hotmail.com>
2022-01-11 00:16:48 +0530

    RangeCountSortUsingPrefixSum

--- C++/2.Searching_Sorting/6.CountSort.cpp
+++ C++/2.Searching_Sorting/6.CountSort.cpp
@@ -40,7 +40,38 @@ void RangeCountSort(int arr[],int n, int hi, int lo){
     }
 
 }
-        
+
+
+//Provides stable sort
+void CountSortUsingPrefixSum(int arr[],int n, int hi, int lo){
+
+    int fmap[hi-lo+1];
+    memset(fmap, 0, sizeof(fmap));
+
+    //1.Generate frequency map
+    for(int i=0; i<n; i++){
+        int index=arr[i];
+        fmap[index-lo]++;
+    }
+
+    //2.Generate generic prefixsum array
+    fmap[0]--;
+    for(int i=1; i<n; i++){
+        fmap[i]+=fmap[i-1];
+    }
+
+    //3.Add elements in a new array from back. Also decrease the count of fmap[i] after every iteration.
+    int narr[n];
+    for(int i=n-1; i>=0; i--){
+        narr[fmap[arr[i]-lo]] = arr[i];
+        fmap[arr[i]-lo]--;
+    }
+
+    for(int i=0; i<n; i++){
+        arr[i]=narr[i];
+    }
+
+} 
    
 
 int main(){
@@ -54,7 +85,9 @@ int main(){
 
     int lo,hi; cin>>lo>>hi;
     //CountSort(arr, arrsize, hi);
-    RangeCountSort(arr, arrsize, hi, lo);
+    //RangeCountSort(arr, arrsize, hi, lo);
+    CountSortUsingPrefixSum(arr, arrsize, hi, lo);
+
 
     for(int i=0; i<(sizeof(arr)/sizeof(int)); i++){
         cout<<arr[i]<<" ";
