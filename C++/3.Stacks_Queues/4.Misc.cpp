b4c0536624735ca8f7cf5a999b8a153a7b0ae640
AjayJam97 <jamwal_ajay@hotmail.com>
2022-02-10 12:40:35 +0530

    MergeOverlapping

--- C++/3.Stacks_Queues/4.Misc.cpp
+++ C++/3.Stacks_Queues/4.Misc.cpp
@@ -42,19 +42,64 @@ void celebrityProblem(vector<vector<int>> arr){
     cout<<potentialCeleb<<endl;
 }
 
+
+void MergeOverlappingInterval(vector<pair<int,int>> arr){
+
+    sort(arr.begin(),arr.end());
+    stack<pair<int,int>> s;
+    s.push(arr[0]);
+    
+    for(int i=1;i<arr.size();i++){
+        
+        pair<int,int> p = arr[i];
+
+        if(p.first <= s.top().second){
+            //second may get updated
+            if(p.second > s.top().second){
+                s.top().second = p.second;
+            }
+        }else{
+            s.push(p);
+        }
+    }
+
+    stack<pair<int,int>> rs;
+    while(s.size() > 0){
+        rs.push(s.top()); s.pop();
+    }
+
+    while(rs.size() > 0){
+        pair<int,int> res = rs.top(); rs.pop();
+        cout<<res.first<<" "<<res.second<<endl;
+    }
+
+
+
+}
+
 int main(){
  
     int arrsize; cin>>arrsize;
+    vector<pair<int,int>> p;
     //vector<int> arr(arrsize);
     //for(int i=0; i<arr.size(); i++) cin>>arr[i];
 
-    vector<vector<int>> arr(arrsize, vector<int>(arrsize));
-    for(int i=0; i<arr.size(); i++){
-        for(int j=0; j<arr[i].size(); j++) cin>>arr[i][j];
-    }
+    // vector<vector<int>> arr(arrsize, vector<int>(arrsize));
+    // for(int i=0; i<arr.size(); i++){
+    //     for(int j=0; j<arr[i].size(); j++) cin>>arr[i][j];
+    // }
 
+    for(int i=0;i<arrsize;i++){
+    int strt,end; cin>>strt; cin>>end;
+    p.push_back(make_pair(strt,end));
+    }
+    
     //celebrityProblem(arr);
+    MergeOverlappingInterval(p);
+    
 
 
     //for(int i=0; i<arr.size(); i++) cout<<arr[i]<<" ";
 }
+
+
