6ae5714eb9ba2ef2a490b39770a9c888821cbb78
AjayJam97 <jamwal_ajay@hotmail.com>
2022-02-16 20:28:56 +0530

    Rotate

--- C++/4.Array_Strings_Recursion_BackTracking/1.ArrayBasic.cpp
+++ C++/4.Array_Strings_Recursion_BackTracking/1.ArrayBasic.cpp
@@ -68,12 +68,32 @@ struct Pair{
         
     }
 
-
+    void reverseArray(vector<int>& nums, int beg, int end){   
+        while(beg<end){
+            int temp = nums[beg];
+            nums[beg++]=nums[end];
+            nums[end--]=temp;
+        }
+    }
+    
+    void rotateArray(vector<int>& nums, int k) {
+        
+        k=k%nums.size();
+        if(k<0) k=k+nums.size();
+        
+        reverseArray(nums,0,nums.size()-k-1);
+        reverseArray(nums,nums.size()-k,nums.size()-1);
+        reverseArray(nums,0,nums.size()-1);
+    }
 
 int main(){
         
-       string s="the sky is blue";
-       cout<<reverseWords(s);
+    //    string s="the sky is blue";
+    //    cout<<reverseWords(s);
+
+    vector<int> nums = {1,2,3,4,5}; int k=2;
+    rotateArray(nums,k);
+    for(int i=0; i<nums.size(); i++) cout<<nums[i]<<" ";
         
 
 }
