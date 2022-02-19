cff39d20142439f7157bd709880960a7841fb0d8
AjayJam97 <jamwal_ajay@hotmail.com>
2022-02-19 19:18:59 +0530

    SubArray or SubString

--- C++/4.Array_Strings_Recursion_BackTracking/2.SubArrayOrSubString.cpp
+++ C++/4.Array_Strings_Recursion_BackTracking/2.SubArrayOrSubString.cpp
@@ -52,16 +52,16 @@ void MaxSumSubArray(vector<int> v){
 
 
     ////KADANE'S ALGORITHM O(n)
-    int currentSum = 0;
+    // int currentSum = 0;
 
-    for(int i=0; i<v.size(); i++){
-        currentSum+=v[i];
-        if(currentSum<0){
-            currentSum=0;
-        }
-        MaxSum=max(MaxSum,currentSum);
-    }
-    cout<<MaxSum<<endl;
+    // for(int i=0; i<v.size(); i++){
+    //     currentSum+=v[i];
+    //     if(currentSum<0){
+    //         currentSum=0;
+    //     }
+    //     MaxSum=max(MaxSum,currentSum);
+    // }
+    // cout<<MaxSum<<endl;
 
 
 }
@@ -70,31 +70,22 @@ void MaxSumSubArray(vector<int> v){
 
 int main(){
 
-    //Subarray: Contigious sequence of elements within an array
-
+    //Subarray: Contigious sequence of elements within an array. 
     //Eg1: {1,2,1} = {1}, {1,2}, {1,2,1}, 
     //               {2}, {2,1}
 
     //Eg2: {1,2,3} = {1}, {1,2}, {1,2,3}, 
     //               {2}, {2,3},
     //               {3}
-
+    
     vector<int> v = {10,20,30,40};
     //PrintSubArray(v);
-    MaxSumSubArray(v);
+    //MaxSumSubArray(v);
 
 
     //Substring: Same as subarray but in context of a string
     //Eg: "ara" = "a", "r", "ar", "ra", "ara", ""
 
 
-    //Subsequence: A sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements
-    //(Not contigious but order is preserver i.e. <a,a,b> & <b,a,a> are not preserving the order)
-    //Eg: <a,b,a> = <a>, <b>, <a,b> , <b,a>, <a,a>, <a,b,a>, <>
-
-
-    //Subset: A set is subset of another set if all its elements are contained by that set.
-    //(Do not need to be contigous & donot even preserve element order)
-    //(Unlike subarray, substring, subsequence sets do not allow duplicates)
-    //Eg: {1,2,3} = {1}, {2}, {3}, {1,2}, {1,3}, {2,3}, {1,2,3}, {}
+ 
 }
