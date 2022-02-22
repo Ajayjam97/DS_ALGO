a778b55c7094128f5d42c4c0e347bfa2a9da4de9
AjayJam97 <jamwal_ajay@hotmail.com>
2022-02-22 21:32:00 +0530

    Permutation

---
+++ C++/4.Array_Strings_Recursion_BackTracking/5.Permutation.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+void PrintPermutation(string s,string output){
+
+    if(s.size()<1){
+        cout<<output<<endl;
+        return;
+    }
+
+
+    for(int i=0; i<s.size(); i++){
+        PrintPermutation(s.substr(0,i)+s.substr(i+1, s.size()-1), output+s[i]);
+    }
+
+}
+
+int main(){
+
+    PrintPermutation("ABC", "");
+
+}
