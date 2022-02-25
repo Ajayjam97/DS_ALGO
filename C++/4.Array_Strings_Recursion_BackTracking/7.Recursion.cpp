f968a4359dfd5a0df3cef649cc588bc2441018ba
AjayJam97 <jamwal_ajay@hotmail.com>
2022-02-26 01:07:47 +0530

    Factorial

--- C++/4.Array_Strings_Recursion_BackTracking/7.Recursion.cpp
+++ C++/4.Array_Strings_Recursion_BackTracking/7.Recursion.cpp
@@ -11,9 +11,19 @@ void printIncDec(int n){
     
 }
 
+int factorial(int n){
+
+    if(n==0) return 1;
+    
+    int ans=n*factorial(n-1);
+    return ans;
+    
+}
+
 int main(){
 
     int n; cin>>n;
-    printIncDec(n);
+    //printIncDec(n);
+    factorial(n);
 
 }
