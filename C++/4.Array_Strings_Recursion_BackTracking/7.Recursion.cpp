cf626b182540e30b39da399fc8388001454d3181
AjayJam97 <jamwal_ajay@hotmail.com>
2022-02-26 01:17:23 +0530

    Logarithmic Power

--- C++/4.Array_Strings_Recursion_BackTracking/7.Recursion.cpp
+++ C++/4.Array_Strings_Recursion_BackTracking/7.Recursion.cpp
@@ -22,8 +22,16 @@ int factorial(int n){
 
 int power(int x,int n){
   
-   if(n==0) return 1;
-   return x*power(x,n-1);
+    //LINEAR POWER
+    // if(n==0) return 1;
+    // return x*power(x,n-1);
+
+    //LOGARTIHMIC POWER
+    if(n==0) return 1;
+    int ans=power(x,n/2);
+    
+    if(n%2!=0) return ans*ans*x;
+    return ans*ans;
 
 }
 
