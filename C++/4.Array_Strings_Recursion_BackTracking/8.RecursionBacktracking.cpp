376f939ad5ff7a3071411b1cea21d9f8d9328925
AjayJam97 <jamwal_ajay@hotmail.com>
2022-03-02 21:02:33 +0530

    Target sum subset

--- C++/4.Array_Strings_Recursion_BackTracking/8.RecursionBacktracking.cpp
+++ C++/4.Array_Strings_Recursion_BackTracking/8.RecursionBacktracking.cpp
@@ -41,21 +41,78 @@ void PrintQueenPaths(vector<vector<int>> arr){
     
 }
 
+vector<pair<int,int>> dirpoints={ {-1,0}, {0,-1}, {1,0}, {0,1} };
+vector<char>          direction={'t',  'l',  'd',  'r'};
+
+void FloodFill(vector<vector<int>> maze, int sr, int sc, string asf) {
+//write your code here
+
+    if(sr==maze.size()-1&&sc==maze[0].size()-1){
+        cout<<asf<<endl;
+        return;
+    }
+
+    //MARK: Set current visited coordiantes to 1
+    maze[sr][sc]=1;
+
+    for(int i=0; i<direction.size(); i++){
+        //Calculate x & y coordinates for one of the 4 directions
+        int x=sr+dirpoints[i].first; 
+        int y=sc+dirpoints[i].second; 
+        //Make recursive to next valid direction. Check if the deirection is not out of bounds & not pre visited/blocked
+        if(x>=0&&x<maze.size()&&y>=0&&y<maze[0].size()&&maze[x][y]!=1)
+        FloodFill(maze,x,y,asf+direction[i]);
+    }
+
+    //UNMARK: Unset visited coordinate to 0 when backtracking
+    maze[sr][sc]=0;
+
+}
+
+void TargetSumSubsets(vector<int> arr, int idx, string set, int subsetsum, int target) {
+    
+    if(idx==arr.size()){
+        if(subsetsum==target)
+        cout<<set<<"."<<endl;
+        return;
+    }
+   
+    if(subsetsum+arr[idx]<=target)
+    TargetSumSubsets(arr, idx+1, set+to_string(arr[idx])+", ", subsetsum+arr[idx], target);
+
+
+    TargetSumSubsets(arr, idx+1, set, subsetsum, target);
+    
+}
+
+
 int main(){
 
-        vector<vector<int>> arr(3,vector<int>(3));
+        //vector<vector<int>> arr(3,vector<int>(3));
+        // arr = {
+        //         {0,0,0,0,0,0,0},
+        //         {0,0,0,0,0,0,0},
+        //         {0,0,0,0,0,0,0},
+        //         {0,0,0,0,0,0,0},
+        //         {0,0,0,0,0,0,0},
+        //         {0,0,0,0,0,0,0},
+        //         {0,0,0,0,0,0,0}
+        //     };
+        //PrintQueenPaths(arr);
 
-        arr = {
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0}
-            };
 
-        PrintQueenPaths(arr);
+        // vector<vector<int>> maze(3,vector<int>(3));
+        // maze = {
+        //         {0,0,0},
+        //         {1,0,1},
+        //         {0,0,0}
+        //     };
+        // FloodFill(maze, 0, 0, "");
+
+
+        vector<int> arr = {10,20,30,40,50};
+        int target=60;
+        TargetSumSubsets(arr, 0, "", 0, target);
 
 
 }
