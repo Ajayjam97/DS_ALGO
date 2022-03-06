9be29a851ada98995f898e9b23464309a38e808b
AjayJam97 <jamwal_ajay@hotmail.com>
2022-03-06 11:54:24 +0530

    Sudoku Solver

--- C++/4.Array_Strings_Recursion_BackTracking/8.RecursionBacktracking.cpp
+++ C++/4.Array_Strings_Recursion_BackTracking/8.RecursionBacktracking.cpp
@@ -222,6 +222,41 @@ void printKnightsTour(vector<vector<int>>& chess, int n, int r, int c, int upcom
 
 }
 
+bool isValid(vector<vector<char>>& board, int row, int col, char val){
+        
+        for(int i=0; i<9; i++){
+            if(board[row][i]==val) return false;
+            if(board[i][col]==val) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==val) return false;        
+        }
+        return true;
+    }
+    
+bool solve(vector<vector<char>>& board){
+        
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[0].size(); j++){
+            
+                if(board[i][j]=='.'){
+                    
+                    for(char val='1'; val<='9'; val++){
+                        if(isValid(board, i, j, val)){
+                            board[i][j]=val;
+                            if(solve(board)) return true;
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                    
+                }
+                
+            }
+        }
+        
+        return true;
+        
+    }
+
 int main(){
 
         //vector<vector<int>> arr(3,vector<int>(3));
