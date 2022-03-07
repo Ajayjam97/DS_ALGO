149ff6378ade4a20f9455e19b1c8bf31e389f8cb
AjayJam97 <jamwal_ajay@hotmail.com>
2022-03-08 00:20:47 +0530

    Min cost in maze traversal

--- C++/5.DP/1.BasicDP.cpp
+++ C++/5.DP/1.BasicDP.cpp
@@ -209,7 +209,7 @@ int MinJumps_tabulation(int i, int n, vector<int>& jumps)
     // return dp[i]=minmove+1;
 }
 
-int minCostClimbingStairs_rec(vector<int> &cost, int idx){
+int minCostClimbingStairsHelper_Rec(vector<int> &cost, int idx){
 	        
         if(idx>=cost.size()) {
             return 0;
@@ -218,7 +218,7 @@ int minCostClimbingStairs_rec(vector<int> &cost, int idx){
         int mincost=1e9;
 	    for(int jump=1; jump<=2; jump++){
 		
-		    int newmincost=minCostClimbingStairs_rec(cost, idx+jump);
+		    int newmincost=minCostClimbingStairsHelper_Rec(cost, idx+jump);
             
             if(idx+jump<cost.size())
             newmincost+=cost[idx+jump];
@@ -230,9 +230,9 @@ int minCostClimbingStairs_rec(vector<int> &cost, int idx){
 	    return mincost;
     }
 
-int minCostClimbingStairs(vector<int>& cost){
+int minCostClimbingStairs_Rec(vector<int>& cost){
 	    
-        int result=minCostClimbingStairs_rec(cost, -1);
+        int result=minCostClimbingStairsHelper_Rec(cost, -1);
 	    return result;
 }
 
@@ -267,12 +267,75 @@ int minCostClimbingStairs_DP(vector<int>& cost){
 	    return result;
 }
 
+int mazeTraversal(int sr, int sc, int dr, int dc, vector<vector<int>> arr){
+    
+    if((sr==dr)&&(sc==dc)){ return arr[sr][sc]; }
+
+    int mincost = (int)1e9;
+
+    if(sc+1 <= dc)
+    mincost = min(mincost,mazeTraversal(sr, sc+1, dr, dc, arr));
+    if(sr+1 <= dr)
+    mincost = min(mincost,mazeTraversal(sr+1, sc, dr, dc, arr));
+
+    return (mincost + arr[sr][sc]);
+
+}
+
+int mazeTraversalmemo(int sr, int sc, int dr, int dc, vector<vector<int>> arr, vector<vector<int>> dp){
+    
+    if((sr==dr)&&(sc==dc)){ return dp[sr][sc] = arr[sr][sc]; }
+
+    int mincost = (int)1e9;
+
+    if(dp[sr][sc]!=0){
+        return dp[sr][sc];
+    }
+
+    if(sc+1 <= dc)
+    mincost = min(mincost,mazeTraversalmemo(sr, sc+1, dr, dc, arr, dp));
+    if(sr+1 <= dr)
+    mincost = min(mincost,mazeTraversalmemo(sr+1, sc, dr, dc, arr, dp));
+
+    return dp[sr][sc]=(mincost + arr[sr][sc]);
+
+
+}
+
+int mazeTraversaltabulated(int sr, int sc, int dr, int dc, vector<vector<int>> arr, vector<vector<int>> dp){
+    
+    for(int i = dr; i>=0; i--){
+        for(int j = dc; j>=0; j--){
+
+            if(i==dr && j==dc){
+                dp[i][j] = arr[i][j];
+                continue;
+            }
+
+            int mincost = (int)1e9;
+
+            if(j+1 <= dc)
+            mincost = min(mincost,dp[i][j+1]);
+            
+            if(i+1 <= dr)
+            mincost = min(mincost,dp[i+1][j]);
+
+            dp[i][j]=(mincost + arr[i][j]);
+
+        }
+    }
+
+    return dp[0][0];
+
+   
+}
+
 int main(){
 
     //vector<int> dp(7, 0);
     //cout<<fib_recursion(6)<<endl<<fib_memoization(6,dp)<<endl<<fib_Tabulation(6)<<endl; //0 1 1 2 3 5 8 13 ................
   
     vector<int> v = {10, 15, 20};
-    cout<<minCostClimbingStairs(v);
+    cout<<minCostClimbingStairs_DP(v);
 
 }
