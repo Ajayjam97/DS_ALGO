3a7a3678253bdb7d8eca91d7178865f377c187fa
AjayJam97 <jamwal_ajay@hotmail.com>
2022-03-26 01:32:44 +0530

    Equal Partition Subset

--- C++/5.DP/4.ParentProblem_01KnapSack.cpp
+++ C++/5.DP/4.ParentProblem_01KnapSack.cpp
@@ -104,11 +104,6 @@ bool isSubsetSum_tabulated(vector<int> arr, int indx, int sum, vector<vector<int
         for(int indx=dp.size()-1; indx>=0; indx--){
             for(int sum=dp[0].size()-1; sum>=0; sum--){
                 if(indx==dp.size()-1) dp[indx][sum]=0;
-            }
-        }
-
-        for(int indx=dp.size()-1; indx>=0; indx--){
-            for(int sum=dp[0].size()-1; sum>=0; sum--){
                 if(sum==0) dp[indx][sum]=1;
             }
         }
@@ -138,6 +133,126 @@ bool isSubsetSum(vector<int>arr, int sum){
         return isSubsetSum_tabulated(arr, 0, sum, dp);
 }
 
+
+bool isSubsetSumForequalPartition(int arr[], int indx, int sum, vector<vector<int>> &dp){
+        
+        if(indx==dp.size()-1){
+            if(sum==0) return dp[indx][sum]=1;
+            return dp[indx][sum]=0;
+        }
+        
+        if(dp[indx][sum]!=-1) return dp[indx][sum];
+        
+        bool YesCallResult=0;
+        bool NoCallResult=0;
+        
+        if(sum-arr[indx]>=0)
+        YesCallResult=isSubsetSumForequalPartition(arr, indx+1, sum-arr[indx], dp);
+        
+        NoCallResult=isSubsetSumForequalPartition(arr, indx+1, sum, dp);
+    
+        return dp[indx][sum]=YesCallResult||NoCallResult;
+    }
+
+int equalPartition_helper(int N, int arr[], int sum){
+    
+    vector<vector<int>> dp(N+1, vector<int>(sum+1, -1));
+    
+    if(isSubsetSumForequalPartition(arr, 0, sum, dp)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    
+}
+    
+int equalPartition(int N, int arr[])
+{
+    // code here
+    int sum=0;
+    for(int i=0; i<N; i++) sum+=arr[i];
+    if(sum%2==0){
+        return equalPartition_helper(N, arr, sum/2);
+    }
+    else{
+        return 0;
+    }
+}
+
+
+bool TargetSumSubset(int sum, vector<int> arr, int tar){
+
+    if(arr.size()==0){
+    if(sum == tar) return true;
+    else return false;
+    }
+ 
+    int num = arr[arr.size()-1]; arr.pop_back();
+    
+    bool res1 = false;
+    bool res2 = false;
+
+    res1 = TargetSumSubset(sum+num, arr, tar);
+    res2 = TargetSumSubset(sum, arr, tar);
+
+    return (res1 || res2);    
+
+}
+
+bool TargetSumSubsetMemo(vector<int> arr, int index, int tar, vector<vector<bool>> dp) {
+        if(tar == 0) return dp[index][tar] = true; 
+        if(index == 0) {
+            return dp[index][tar] = false;
+        }
+
+        if(dp[index][tar] != false) {
+            return dp[index][tar];
+        }
+
+        bool res = false;
+        // yes call
+        if(tar - arr[index] >= 0) {
+            res = TargetSumSubsetMemo(arr, index - 1, tar - arr[index], dp);
+        }
+        // no call
+        res = res || TargetSumSubsetMemo(arr, index - 1, tar, dp);
+        return dp[index][tar] = res;
+    }
+
+bool TargetSumSubsetTab(vector<int> arr, int tar){
+
+    vector<vector<bool>> dp(arr.size()+1, vector<bool>(tar + 1));
+
+    for(int indx = 0; indx < dp.size(); indx++){
+        for(int target = 0; target < dp[0].size(); target++){
+            if(target == 0){
+                dp[indx][target] = true;
+            }
+            else if(indx == 0){
+                dp[indx][target] = false;
+            }
+            else{
+                int val = arr[indx-1];
+                if(target < val){
+                    //no call
+                    dp[indx][target] = dp[indx-1][target];
+                }            
+                else{
+                    //no || yes call
+                    dp[indx][target] = dp[indx-1][target] || dp[indx-1][target-val];
+
+                } 
+
+            }
+        }
+    }
+
+    return dp[dp.size()-1][dp[0].size()-1];    
+     
+}
+
+
 int main(){
 
     // int wt[]={4,5,1};
@@ -145,7 +260,7 @@ int main(){
     // cout<<zero_oneknapSack(4, wt, val, 3);
 
     vector<int> val={3,34,4,12,5,2};
-    cout<<isSubsetSum(val, 30);
+    cout<<isSubsetSum(val, 9);
 
 }
 
