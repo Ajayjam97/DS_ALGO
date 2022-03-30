6e42c05a5a1f5194c337d44af7a32e4ad14d9e6f
AjayJam97 <jamwal_ajay@hotmail.com>
2022-03-30 22:23:04 +0530

    Longest Common subsequence tabulated

--- C++/5.DP/6.ParentProblem_LCS.cpp
+++ C++/5.DP/6.ParentProblem_LCS.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 //Function to find the length of longest common subsequence in two strings.
 //LCS can be discontinuous
+int LongestCommonSubsequence_tabulated(int x, int y, string s1, string s2, vector<vector<int>> &dp){
+        
+        for(int i=0; i<x; i++){
+            for(int j=0; j<y; j++){
+                if(i==0||j==0) dp[i][j]=0;
+            }
+        }
+        
+        for(int i=1; i<=x; i++){
+            for(int j=1; j<=y; j++){
+               
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                } 
+                else{
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }
+        
+            }
+        }
+        
+        return dp[x][y];
+        
+    }
+
 int LongestCommonSubsequence_memoized(int x, int y, string s1, string s2, vector<vector<int>> &dp){
 
         if(x==0||y==0) return dp[x][y]=0;
@@ -26,6 +51,7 @@ int LongestCommonSubsequence(int x, int y, string s1, string s2)
 {
     vector<vector<int>> dp(x+1, vector<int>(y+1, -1));
     return LongestCommonSubsequence_memoized(x, y, s1, s2, dp);
+    return LongestCommonSubsequence_tabulated(x, y, s1, s2, dp);
     
 }
 
