a7f94e78795c4596cb58f68645004e3fa26403a1
AjayJam97 <jamwal_ajay@hotmail.com>
2022-04-02 23:05:58 +0530

    Longest Common substring tabulated

--- C++/5.DP/6.ParentProblem_LCS.cpp
+++ C++/5.DP/6.ParentProblem_LCS.cpp
@@ -56,14 +56,68 @@ int LongestCommonSubsequence(int x, int y, string s1, string s2)
 }
 
 
+int LongestCommonSubstring_recursive(string X, string Y,int x, int y, vector<vector<int>> &dp, int count)
+{
+
+    if (x == 0 || y == 0)
+        return count;
+        
+
+    if (X[x - 1] == Y[y - 1]) {
+        count = LongestCommonSubstring_recursive(X, Y, x - 1, y - 1, dp, count + 1);
+    }
+    count = max(count,
+                max(LongestCommonSubstring_recursive(X, Y, x, y - 1, dp, 0),
+                    LongestCommonSubstring_recursive(X, Y, x - 1, y, dp, 0)));
+    return count;
+}
+
+int LongestCommonSubstring_tabulated(string X, string Y,int x, int y, vector<vector<int>> &dp, int count)
+{
+
+    for(int i=0; i<x; i++){
+            for(int j=0; j<y; j++){
+                if(i==0||j==0) dp[i][j]=0;
+            }
+        }
+        
+        for(int i=1; i<=x; i++){
+            for(int j=1; j<=y; j++){
+               
+                if(X[i-1]==Y[j-1]){
+                     dp[i][j]=dp[i-1][j-1]+1;
+                } 
+                else{
+                    dp[i][j]=0;
+                }
+                    
+        
+            }
+        }
+        
+        for(int i=0; i<=x; i++){
+            for(int j=0; j<=y; j++){
+                if(dp[i][j]>count){
+                    count=dp[i][j];
+                }
+            }
+        }
+        
+        return count;
+}
+
 int LongestCommonSubstring (string S1, string S2, int n, int m)
 {
-    // your code here
+    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+    return LongestCommonSubstring_recursive(S1, S2, n, m, dp, 0);
+    return LongestCommonSubstring_tabulated(S1, S2, n, m, dp, 0);
 }
+
 
 
 int main() {
 
     cout<<LongestCommonSubsequence(6,6,"ABCDGH","AEDFHR")<<endl;
+    cout<<LongestCommonSubstring("ABCDGH","ACDGHR",6,6)<<endl;
 
 }
