243577557778a9e6e596c7e4172a99756f40ecde
AjayJam97 <jamwal_ajay@hotmail.com>
2022-04-08 22:14:28 +0530

    Palindrome partitioning

--- C++/5.DP/7.ParentProblem_MCM.cpp
+++ C++/5.DP/7.ParentProblem_MCM.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int mcm(int i, int j, int arr[]){
+int mcm_rec(int i, int j, int arr[]){
         
         if(i==j) return 0;
         
         int mini=1e9;
         
         for(int k=i+1; k<=j; k++){
-            int steps = arr[i-1]*arr[k-1]*arr[j] + mcm(i, k-1, arr) + mcm(k, j, arr);
+            int steps = arr[i-1]*arr[k-1]*arr[j] + mcm_rec(i, k-1, arr) + mcm_rec(k, j, arr);
             if(steps<mini) mini=steps;
         }
         
@@ -16,11 +16,36 @@ int mcm(int i, int j, int arr[]){
         
     }
     
+int mcm_memoized(int i, int j, int arr[], vector<vector<int>> &dp){
+        
+        if(i==j) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int mini=1e9;
+        
+        for(int k=i+1; k<=j; k++){
+            int steps = arr[i-1]*arr[k-1]*arr[j] + mcm_memoized(i, k-1, arr, dp) + mcm_memoized(k, j, arr, dp);
+            if(steps<mini) mini=steps;
+        }
+        
+        return dp[i][j]=mini;
+        
+    }
+  
 int matrixMultiplication(int N, int arr[])
     {
         // code here
-        return mcm(1, N-1, arr);
+        vector<vector<int>> dp(N+1, vector<int>(N+1, -1));
+        return mcm_memoized(1, N-1, arr, dp);
+        return mcm_rec(1, N-1, arr);
     }
+
+
+int palindromicPartition(string str)
+{
+    // code here
+}
 
 int main() {
 
