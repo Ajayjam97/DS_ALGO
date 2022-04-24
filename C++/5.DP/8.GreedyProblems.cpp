24d2c8c81548eea829dfb65ff133496dd6321f09
AjayJam97 <jamwal_ajay@hotmail.com>
2022-04-24 21:15:14 +0530

    Count a+b+c subsequences

--- C++/5.DP/8.GreedyProblems.cpp
+++ C++/5.DP/8.GreedyProblems.cpp
@@ -91,13 +91,35 @@ int ArrangeBuildings(int n){
     return result*result;
 }
 
+int countabcsubsequences(string str){
 
-int countabcsubsequences(){
-    
+    int a=0;
+    int ab=0;
+    int abc=0;
+
+    for(int i=0; i<str.size(); i++){
+        char ch=str[i];
+
+        if(ch=='a'){
+            a=2*a+1;
+        }
+        
+        else if(ch=='b'){
+            ab=2*ab+a;
+        }
+        
+        else if(ch=='c'){
+            abc=2*abc+ab;
+        }
+    }
+
+    return abc;
 }
 
 int main() {
 
 cout<<countBinaryString(0,1,"",4)<<endl;
+
+cout<<countabcsubsequences("abcabc")<<endl;
 
 }
