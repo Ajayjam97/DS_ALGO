a73961d27c5d0c54f0665514fb96062cdef6dcf6
AjayJam97 <jamwal_ajay@hotmail.com>
2022-07-08 23:06:32 +0530

    Path to leaf from root in range

---
+++ C++/8.Trees/2.Binary/7.PathToLeafFromRootInRange.cpp
@@ -0,0 +1,134 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Node {
+
+public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) {
+            this->data = data;
+            this->left = this->right = NULL;
+        }
+
+        Node(int data, Node* left, Node* right) {
+            this->data = data;
+            this->left = left;
+            this->right = right;
+        }
+};
+
+class btree{
+
+public:
+
+    class Pair {
+    public:
+        Node* node;
+        int state;
+
+        Pair(Node* node, int state) {
+            this->node = node;
+            this->state = state;
+        }
+    };
+
+    static Node* construct(vector<string> arr) {
+        Node* root = new Node(stoi(arr[0]));
+        stack<Pair*> st;
+        st.push(new Pair(root, 0));
+
+        int indx = 0;
+        while (st.size() > 0) {
+            Pair* p = st.top();
+            if (p->state == 0) {
+                // left child processing
+                indx++;
+                if (arr[indx] != "n") {
+                    Node* nn = new Node(stoi(arr[indx]));
+                    p->node->left = nn;
+                    st.push(new Pair(nn, 0));
+                }
+                p->state++;
+            } else if (p->state == 1) {
+                // right child processing
+                indx++;
+                if (arr[indx] != "n") {
+                    Node* nn = new Node(stoi(arr[indx]));
+                    p->node->right = nn;
+                    st.push(new Pair(nn, 0));
+                }
+                p->state++;
+            } else {
+                // pop out node-pair from stack
+                st.pop();
+            }
+        }
+        return root;
+    }
+
+    static void display(Node* root) {
+        
+       if (root == NULL)
+            return;
+
+        string str = (root->left == NULL) ? " ." : "" + to_string(root->left->data);
+        str += " <- [" + to_string(root->data) + "] -> ";
+        str += (root->right == NULL) ? ". " : to_string(root->right->data);
+        cout<<str<<endl;
+
+        display(root->left);
+        display(root->right);
+    }
+
+
+
+
+    static void pathToLeafFromRoot(Node* node, string path, int sum, int lo, int hi) {
+        if(node == NULL) return;
+
+        if(node->left != NULL && node->right != NULL) {
+            pathToLeafFromRoot(node->left, path + to_string(node->data) + " ", sum + node->data, lo, hi);
+            pathToLeafFromRoot(node->right, path + to_string(node->data) + " ", sum + node->data, lo, hi);
+        } else if(node->left != NULL) {
+            pathToLeafFromRoot(node->left, path + to_string(node->data) + " ", sum + node->data, lo, hi);
+        } else if(node->right != NULL) {
+            pathToLeafFromRoot(node->right, path + to_string(node->data) + " ", sum + node->data, lo, hi);
+        } else {
+            // leaf
+            sum += node->data;
+            path += to_string(node->data);
+            if(lo <= sum && sum <= hi) {
+                // print path
+                cout<<path<<endl;
+            }
+        }
+    }
+ 
+
+};
+
+
+int main(){
+
+int n; cin>>n;
+vector<string> v(n);
+
+for(int i=0; i<v.size(); i++){
+    cin>>v[i];
+}
+
+int lo; cin>>lo;
+int hi; cin>>hi;
+Node* root = btree::construct(v);
+
+btree::pathToLeafFromRoot(root, "", 0, lo, hi);
+
+
+}
