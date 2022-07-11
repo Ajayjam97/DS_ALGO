a57e5e9aa2226eb2676dfea40d9c9dd8a9b04b7b
AjayJam97 <jamwal_ajay@hotmail.com>
2022-07-11 22:06:55 +0530

    Sort K Sorted

--- C++/9.HeapAndHashmap/3.SortKSortedArray.cpp
+++ C++/9.HeapAndHashmap/3.SortKSortedArray.cpp
@@ -5,20 +5,22 @@
 using namespace std;
 
 
-void PrintKElements(vector<int> v,int k){
+void PrintKSorted(vector<int> v,int k){
 
     priority_queue<int, vector<int>, greater<int>> pq;
 
     //Add k elements
     for(int i=0; i<k; i++){
-        pq.emplace(v[i]); 
+        pq.emplace(v[i]);
     }
+
     //Process remaining elements
     for(int i=k; i<v.size(); i++){
-        if(v[i] > pq.top()){
-            pq.pop();
-            pq.emplace(v[i]);
-        }
+        //add
+        pq.emplace(v[i]);
+        //remove
+        cout<<pq.top()<<endl;
+        pq.pop();
     }
 
     //Print k elements from priority queue
@@ -41,7 +43,7 @@ int main(){
       }
    
     cin>>k;
-    PrintKElements(v, k);
+    PrintKSorted(v, k);
 
 
     return 0;
