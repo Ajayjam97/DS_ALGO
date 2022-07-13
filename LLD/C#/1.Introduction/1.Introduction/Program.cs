75a8c81389a38e3e0f100b46d52b3f4626f730c7
AjayJam97 <jamwal_ajay@hotmail.com>
2022-07-14 00:37:11 +0530

    Introduction

---
+++ LLD/C#/1.Introduction/1.Introduction/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Threading;
+
+namespace _1.Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //.Net Framework consists of CLR(Command Language Runtime) & FCL(Framework Class Library)
+
+            //CLR is the runtime environment that runs the codes & helps in making development process easier by providing  
+            //services like remoting, thread management, type-safety etc.
+
+            //CLR further contains CLI(Command Language Infrastructure), GC(Garbage Collector) & JIT(Just In Time compiler)
+
+            //FCL is a collection of reusable, object-oriented class libraries and methods, etc that can be integrated with CLR. 
+
+            //Namespaces in FCL are a group of related classes and interfaces that can be used by all .NET supported languages.
+            //Ex: System, System.Collections, System.IO, System.Threading etc
+
+            //Execution Process of CLR : Source code > MSIL(Microsoft Integrated Language) code > Object/Executable
+            //Stages of code           : C# code     > Compiletime (Language specific compiler) > Runtime (JIT)
+
+            //Managed Vs Unmanaged code
+            //Managed code executed by a managed runtime environment. Pointers can not be used in managed code because of GC.
+            //Unmanaged code executed directly by OS. Unsafe code provides the flexibilty to use pointers.
+
+            unsafe{
+                int var = 20;
+                int* p = &var;
+
+                Console.WriteLine("Value of *p is: {0}", *p);
+            }
+
+            //Some differences in C# & Java
+
+            //C# supports operator overloading & Java does not.
+            //C# has CLR & Java has JVM.
+            //C# doesnot supports checked exceptions & Java supports both checked, unchecked exceptions.
+            //C# supports structure, Union & Java doesnot supports both.
+
+
+            //Features of C#:
+            //Simple, Modern, Consistent, Object Oriented, Type Safe, Compatible, Interoperable.
+        }
+    }
+}
---
+++ LLD/C#/2.LexicalConventions/2.LexicalConventions/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _2.LexicalConventions
+{
+    class Program
+    {
+        public readonly int var1 = 10;
+        public const int var2 = 10;
+
+        public Program(int var1, int var2){
+            this.var1 = var1;
+            //this.var2 = var2;
+
+            Console.WriteLine("Var1: {0}", this.var1);
+            //Console.WriteLine("Var2: {0}", this.var2);
+        }
+
+
+        public static void RefGetId(ref int id){
+            id++;
+        }
+
+        public static void OutGetId(int id, out int result){
+            result=id+1;
+        }
+
+        static void Main(string[] args)
+        {
+
+            //A C# program is a collection of classes.
+            //A class is defined by a set of object ,variable and methods.A c# program is basically collection of tokens.
+            
+            //79 Keywords are there in C#
+            //abstract    as
+            //base        bool            break            byte
+            //case        catch           char             checked            class            const            continue
+            //decimal     default         delegate         do                 double
+            //else        enum            event            explicit           extern
+            //false       finally         fixed            float              for              foreach
+            //goto
+            //if          implicit        in               int                interface        internal         is
+            //lock        long
+            //namespace   new             null
+            //object      operator        out              override
+            //params      private         protected        public
+            //readonly    ref             return
+            //sbyte       sealed          short        sizeof     stackalloc      static       string       struct       switch
+            //this        throw           true         try        typeof
+            //uint        ulong           unchecked    unsafe     ushort          using
+            //virtual     void            volatile
+            //while
+
+
+            //Difference between ref & out
+
+            //int id=0;
+            //RefGetId(ref id);
+            //Console.WriteLine(id);
+
+            //int result=0;
+            //OutGetId(id, out result);
+            //Console.WriteLine(result);
+
+
+
+            //Difference between Const VS ReadOnly
+
+            Program P = new Program(20, 20);
+
+        }
+    }
+}
