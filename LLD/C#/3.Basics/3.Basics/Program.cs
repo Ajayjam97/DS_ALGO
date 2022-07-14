49c7eaee46da3eb4cd1bd251e599c1cd268a2b6f
AjayJam97 <jamwal_ajay@hotmail.com>
2022-07-15 00:44:55 +0530

    Arrays, Strings, String Builders

--- LLD/C#/3.Basics/3.Basics/Program.cs
+++ LLD/C#/3.Basics/3.Basics/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Collections;
 
 namespace _3.Basics
 {
@@ -13,6 +14,13 @@ namespace _3.Basics
         Friday    = 5,  // 5
         Saturday  = 6,  // 6
         Sunday    = 7  // 7
+    }
+
+    
+    struct ABC
+    {
+        int a;
+        public string b;
     }
 
     class Program
@@ -34,13 +42,51 @@ namespace _3.Basics
             System.Object    @object = "Boy"; Console.WriteLine("Value of Object: {0}", @object);//Object
             System.String    @string = "ImmutableString"; Console.WriteLine("Value of Object: {0}", @string);//String
             System.Text.StringBuilder    @stringbuilder = new StringBuilder("MutableString"); Console.WriteLine("Value of Object: {0}", @stringbuilder);//String
-    
+
+
+            //Enum
             Days @monday = Days.Monday;     Console.WriteLine("Value of Object: {0}", (int)@monday);//Enum
+
+
+            //Struct
+            ABC abc;
+            //abc.a=10;     //Private member
+            abc.b="ABC";    //Public member
+            Console.WriteLine("abc.b = {0}",abc.b);
+
+            //Differnece between Classes & structs
+            //Calsses are reference types, structs are value types
+            //Classes support inheritance, structs do not support inheritance
 
             //Boxing Vs Unboxing
             int num=23;
             object obj = num;   Console.WriteLine("Value of obj: {0}", obj);//Boxing
             int i = (int) obj;  Console.WriteLine("Value of i: {0}", i);//Unboxing
+
+
+            //Arrays: A group of contigious or common data items that share a common name.
+            int[] numbers = new int[5];      for(int idx=0; idx<numbers.Length; idx++){ numbers[idx]=idx; Console.WriteLine(numbers[idx]);};
+            float[] price = new float[2];   
+            int[,] TwoDarray = new int[2,2];
+
+            for(int x=0; x<2; x++){
+                for(int y=0; y<2; y++){ TwoDarray[x,y] = x*y;   Console.WriteLine("Value in 2D array: {0}",TwoDarray[x,y]);}
+            } 
+
+
+            //ArrayList: A collection that can store dynamically sized array of objects.An arraylist is very similar to 
+            //an array except the fact that it can grow dynamically.
+
+            ArrayList cities = new ArrayList();
+            cities.Add("Jammu");    cities.Add("Bangalore");
+            Console.WriteLine("Arralist Capacity = {0}, Arraylist Count = {1}", cities.Capacity, cities.Count);
+
+            //C# supports 2 types of Strings(Mutable(StringBuilder) & Immutable(System.String))
+            string str="abc";
+            StringBuilder strbuilder = new StringBuilder("cde");
+
+
+
 
         }
     }
