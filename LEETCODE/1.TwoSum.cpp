f7f3f04c5ab8dde1012e43b646d4be07291087d9
AjayJam97 <jamwal_ajay@hotmail.com>
2022-07-18 19:56:08 +0530

    LC Questions update

---
+++ LEETCODE/1.TwoSum.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+     vector<int> nums={3,2,4};  int target=6;
+     vector<int> result;
+     unordered_map<int,int> hash;
+
+  
+    for(int i=0; i<nums.size(); i++){
+        
+        if(hash.find(target-nums[i]) != hash.end()){
+            result.push_back(i);
+            result.push_back(hash[target-nums[i]]);
+            break;
+        }
+        
+        hash[nums[i]]=i;
+        
+    }
+          
+    for(int indx : result){
+        cout<<indx<<" ";
+    }
+
+}
+
+
