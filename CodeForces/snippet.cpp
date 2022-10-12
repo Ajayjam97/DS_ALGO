c2147a8dcdc4c5ff6c67096ec91fa0a8fedd7033
AjayJam97 <jamwal_ajay@hotmail.com>
2022-10-13 05:22:01 +0530

    CP

---
+++ CodeForces/snippet.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Ajay Singh
+Linkedin: https://www.linkedin.com/in/ajay-jamwal97/
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define endl "\n"
+#define mod (int)1000000007
+#define tc(t) int t; cin>>t; while(t--)
+#define init(a,i) memset(a,i,sizeof(a));
+
+int main(){
+
+
+}
